3035ac8bb34dd5a4d74aa987962ec3243d878068
paollamiranda <paollamiranda9@gmail.com>
2022-05-06 22:17:38 -0300

    projeto.2-alterações

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<script>
+import CabecalhoHeader from "@/components/CabecalhoHeader.vue";
+import PrincipalContent from "./components/PrincipalContent.vue";
+
+export default {
+  components: { CabecalhoHeader, PrincipalContent },
+};
+</script>
+<template>
+  <CabecalhoHeader />
+  <PrincipalContent />
+</template>
+<style></style>
